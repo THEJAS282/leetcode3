6db2deac0b9b860c625292c2f5266b66deaa48cc
G Thejas <159606381+THEJAS282@users.noreply.github.com>
2025-09-28 09:56:44 +0530

    Add files via upload

---
+++ longestone.py
@@ -0,0 +1,11 @@
+def longestOnes(self, nums, k):
+        left, maxLength, zeroCount = 0, 0, 0
+        for right in range(len(nums)):
+            if nums[right] == 0:
+                zeroCount += 1
+            while zeroCount > k:
+                if nums[left] == 0:
+                    zeroCount -= 1
+                left += 1
+            maxLength = max(maxLength, right - left + 1)
+        return maxLength
