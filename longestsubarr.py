e15851fe72b9d6a0343dcddd99f636b604fc4a87
G Thejas <159606381+THEJAS282@users.noreply.github.com>
2025-09-28 09:57:06 +0530

    Add files via upload

---
+++ longestsubarr.py
@@ -0,0 +1,20 @@
+def longestSubarray(self, nums):
+        i = 0
+        j = 0
+        n = len(nums)
+        maxi = 0
+        count = 0
+
+        while j < n:
+            if nums[j] == 0:
+                count += 1
+
+            while count > 1:
+                if nums[i] == 0:
+                    count -= 1
+                i += 1
+
+            maxi = max(maxi, j - i)
+            j += 1
+
+        return maxi
