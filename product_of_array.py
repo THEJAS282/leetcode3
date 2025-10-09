00e462f0d14312e1db8ab6765a87bf772a5e8976
G Thejas <159606381+THEJAS282@users.noreply.github.com>
2025-10-09 21:15:39 +0530

    Add files via upload

---
+++ product_of_array.py
@@ -0,0 +1,18 @@
+def productExceptSelf(self, nums):
+        n = len(nums)
+        result = [1] * n
+
+        # Step 1: Prefix product
+        prefix = 1
+        for i in range(n):
+            result[i] = prefix
+            prefix *= nums[i]
+
+        # Step 2: Suffix product
+        suffix = 1
+        for i in range(n - 1, -1, -1):
+            result[i] *= suffix
+            suffix *= nums[i]
+
+        return result
+        
